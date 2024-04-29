2e23bb618cd0eab891b25bb77e772ba0c8907331
piyo <chick.p.1120@gmail.com>
2024-04-29 09:56:48 +0900

    feat(webapp): implement endpoint for deleting location

--- apps/webapp/src/api/locations.ts
+++ apps/webapp/src/api/locations.ts
@@ -1,10 +1,12 @@
 import { zValidator } from "@hono/zod-validator";
 import { drizzle } from "drizzle-orm/d1";
+import { eq } from "drizzle-orm";
 import { Hono } from "hono";
 
 import { locations } from "../schema";
 import { Bindings } from "../types";
 import { LocationScheme } from "../types/location";
+import { z } from "zod";
 
 const app = new Hono<{ Bindings: Bindings }>();
 
@@ -21,5 +23,29 @@ app.get("/locations", zValidator("form", LocationScheme), async (c) => {
     return c.json({ message: "Unknown Error" }, 500);
   }
 });
+
+app.delete(
+  "/locations/:id",
+  zValidator(
+    "param",
+    z.object({
+      id: z.string(),
+    }),
+  ),
+  async (c) => {
+    const id = c.req.param("id");
+    const db = drizzle(c.env.DB);
+    try {
+      await db.delete(locations).where(eq(locations.id, Number(id)));
+      return c.json({ message: `Success to delete (id:${id})` }, 204);
+    } catch (e) {
+      if (e instanceof Error) {
+        console.error(e.message);
+        return c.json({ message: "Internal Error" }, 500);
+      }
+      return c.json({ message: "Unknown Error" }, 500);
+    }
+  },
+);
 
 export default app;
